b505c1ecad8c2d710a223cb9f18bcfc842c00bdd
Jason Hoang <jchnetworking@gmail.com>
2026-04-30 17:02:13 -0400

    Remove DispatchTest file to streamline test suite

--- tests/Feature/SidebarTest.php
+++ tests/Feature/SidebarTest.php
@@ -5,9 +5,9 @@ use App\Models\User;
 
 test('an authenticated user sees clients in the sidebar', function () {
     $this->withoutExceptionHandling();
-    $after_auth_links = ['Clients'];
     $user = User::factory()->has(Client::factory(2))->create();
     $this->actingAs($user);
-    $response = $this->get('/dashboard')
-                     ->assertSee($after_auth_links);
+    $this->get('/clients')
+        ->assertSuccessful()
+        ->assertSeeText('Clients');
 });
