dd4c0df2dfb4f3aabad5b9b0a189e5e27a933591
Jason Hoang <jchnetworking@gmail.com>
2026-05-06 19:24:53 -0400

    Update welcome page background color for improved aesthetics
    - Changed the background color of the welcome page from a light red to black, enhancing visual appeal and consistency with the overall design theme.

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -103,7 +103,7 @@ export default function Welcome({
                                 </li>
                             </ul>
                         </div>
-                        <div className="relative -mb-px aspect-[335/376] w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]">
+                        <div className="relative -mb-px aspect-[335/376] w-full shrink-0 overflow-hidden rounded-t-lg bg-black lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg">
                             <img
                                 src="/images/deployer-logo.png"
                                 alt="Deployer logo"
