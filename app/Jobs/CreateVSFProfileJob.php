886216497c850430fbf43aa3ba48c885cc916c81
jch-coding <jchnetworking@gmail.com>
2026-05-19 13:22:36 -0400

    added more logging messages for create vsf profile

--- app/Jobs/CreateVSFProfileJob.php
+++ app/Jobs/CreateVSFProfileJob.php
@@ -25,7 +25,13 @@ class CreateVSFProfileJob extends BaseTaskJob
     public function handle(): void
     {
         $this->handleSafely(function (): void {
+            $message = 'Creating VSF profile for device '.$this->device->name;
+            Log::info($message);
+            $this->task->processTaskStatusLog($message);
             if (! $this->device->scope_id) {
+                $message = 'No scope id found for device '.$this->device->name.', getting scope id from Central...';
+                Log::info($message);
+                $this->task->processTaskStatusLog($message);
                 $scopeid_response = $this->centralAPIHelper->getScopeIdFromCentral($this->device);
                 if (! $this->applyScopeIdFromHierarchyResponse($scopeid_response)) {
                     return;
@@ -37,6 +43,9 @@ class CreateVSFProfileJob extends BaseTaskJob
                 $this->task->processTaskStatusLog($error_message);
             } else {
                 if (! $this->device->site->scope_id) {
+                    $message = 'No scope id found for site '.$this->device->site->name.', getting scope id from Central...';
+                    Log::info($message);
+                    $this->task->processTaskStatusLog($message);
                     $site_scope_id = $this->centralAPIHelper->get_site_scope_id($this->device->site);
                     if (! $site_scope_id) {
                         $message = 'failed to retrieve scope ID for site '.$this->device->site->name;
@@ -45,18 +54,33 @@ class CreateVSFProfileJob extends BaseTaskJob
 
                         return;
                     }
+                    $message = 'Scope id found for site '.$this->device->site->name.', updating site...';
+                    Log::info($message);
+                    $this->task->processTaskStatusLog($message);
                     $this->device->site->scope_id = $site_scope_id;
                     $this->device->site->save();
                 }
+                $message = 'Creating VSF profile for device '.$this->device->name;
+                Log::info($message);
+                $this->task->processTaskStatusLog($message);
                 $response = $this->centralAPIHelper->post_vsf_profile($this->device);
                 if (! $response->ok()) {
                     $message = 'VSF Profile Creation Failed with error '.$response->json()['message'];
-                    Log::error($response->json('message'));
+                    Log::info($message);
                     $this->task->processTaskStatusLog($message, true);
                     $this->release($this->wait_time * 60);
                 } else {
+                    $message = 'VSF profile created successfully, waiting 10 seconds for scope id to refresh...';
+                    Log::info($message);
+                    $this->task->processTaskStatusLog($message);
                     Sleep::for(10)->seconds();
+                    $message = 'Getting scope id for device '.$this->device->name;
+                    Log::info($message);
+                    $this->task->processTaskStatusLog($message);
                     $scope_id_response = $this->centralAPIHelper->getScopeIdFromCentral($this->device);
+                    $message = 'Scope id found for device '.$this->device->name.', updating device...';
+                    Log::info($message);
+                    $this->task->processTaskStatusLog($message);
                     if (! $this->applyScopeIdFromHierarchyResponse($scope_id_response)) {
                         $message = 'VSF profile created but could not refresh scope ID for '.$this->device->name;
                         Log::error($message);
