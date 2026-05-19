50770550ce968b07688c5fa621ad71b999d1416b
jch-coding <jchnetworking@gmail.com>
2026-05-19 16:02:06 -0400

    added more log infor for configurelaginterface job

--- app/Jobs/ConfigureLagInterfaceJob.php
+++ app/Jobs/ConfigureLagInterfaceJob.php
@@ -26,6 +26,9 @@ class ConfigureLagInterfaceJob extends BaseTaskJob
         $this->handleSafely(function (): void {
             $device = $this->device_interface->device;
             if (! $device->scope_id) {
+                $message = '\nScope ID not found for '.$this->device_interface->device->name;
+                Log::info($message);
+                $this->task->processTaskStatusLog($message);
                 $scopeid_response = $this->centralAPIHelper->getScopeIdFromCentral($device);
                 if (array_key_exists('error', $scopeid_response)) {
                     $message = 'Failed to get scope id for device '.$device->name;
@@ -34,9 +37,15 @@ class ConfigureLagInterfaceJob extends BaseTaskJob
 
                     return;
                 }
+                $message = '\nGot Scope ID from Central for '.$this->device_interface->device->name;
+                Log::info($message);
+                $this->task->processTaskStatusLog($message);
                 $device->scope_id = $scopeid_response[0]['scopeId'];
                 $device->save();
             }
+            $message = '\nCreating LAG for '.$this->device_interface->device->name;
+            Log::info($message);
+            $this->task->processTaskStatusLog($message);
             $response = $this->centralAPIHelper->post_interface_portchannel($this->device_interface);
             if (! $response->ok()) {
                 Log::error($response->json('message'));
@@ -46,6 +55,9 @@ class ConfigureLagInterfaceJob extends BaseTaskJob
                 return;
             }
             if ($this->device_interface->sw_profile) {
+                $message = '\nFound switchport configuration for LAG. Applying switchport configuration to LAG for '.$this->device_interface->device->name;
+                Log::info($message);
+                $this->task->processTaskStatusLog($message);
                 $patch_response = $this->centralAPIHelper->patch_interface_portchannel($this->device_interface);
                 if (! $patch_response->ok()) {
                     $message = 'Failed to apply port profile to interface LAG .'.$this->device_interface->interface.': '.$patch_response->json('message');
@@ -55,7 +67,7 @@ class ConfigureLagInterfaceJob extends BaseTaskJob
                     return;
                 }
             }
-            $message = 'Configured LAG '.$this->device_interface->interface;
+            $message = 'Configured LAG '.$this->device_interface->interface.' for '.$this->device_interface->device->name;
             $this->task->processTaskStatusLog($message);
             $this->task->deviceInterfaces()->find($this->device_interface)->pivot->update(['status' => 'COMPLETED']);
         }, 'Configure LAG interface');
